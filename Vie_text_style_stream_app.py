9357233e7651f2046fd590b4ff7110ac85095ff8
hoanglam372000 <77403479+hoanglam372000@users.noreply.github.com>
2021-12-25 19:23:33 +0700

    Update Vie_text_style_stream_app.py

--- Vie_text_style_stream_app.py
+++ Vie_text_style_stream_app.py
@@ -7,13 +7,13 @@
 
 import model_prediction
 import streamlit as st
-st.markdown("<h1 style='text-align: center; font-size:30px'>VIETNAM NATIONAL UNIVERSITY, HO CHI MINH CITY</h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center;font-size:30px'>UNIVERSITY OF SCIENCE</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size:20px'>VIETNAM NATIONAL UNIVERSITY, HO CHI MINH CITY</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;font-size:20px'>UNIVERSITY OF SCIENCE</h1>", unsafe_allow_html=True)
 #st.markdown('<div align="center">**VIETNAM NATIONAL UNIVERSITY, HO CHI MINH CITY**</div>')
 #st.markdown('<div align="center">**UNIVERSITY OF SCIENCE**</div>')
 #st.image(model_prediction.image)
 #desc = st.markdown('**Author:** *Lam Thai Hoang, Tuan Le Ngoc, Hien Pham Thi Hoai, Huy Nguyen Tien, Son Nguyen Truong*')
-st.markdown("<h1 style='text-align: center;font-size:40px'>Vietnamese Text Style Classification Tool</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;font-size:30px'>Vietnamese Text Style Classification Tool</h1>", unsafe_allow_html=True)
 #st.title('Vietnamese Text Style Classification Tool')
 st.markdown('**Author:** *Lam Thai Hoang, Tuan Le Ngoc, Hien Pham Thi Hoai, Huy Nguyen Tien, Son Nguyen Truong*')
 #st.write(desc)
